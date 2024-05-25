33b26618c89eb06a8237475cb703d95a3ee310bc
CentaureaHO <tangxianda15@outlook.com>
2024-05-25 14:56:19 +0800

    Update

--- Canny_x86/main.cpp
+++ Canny_x86/main.cpp
@@ -16,6 +16,167 @@ using Str    = std::string;
 template <typename T>
 using Vec = std::vector<T>;
 
+Str       ImgPath    = "../Images/";
+Str       OutputPath = "../Output/";
+const int LowThre    = 30;
+const int HighThre   = 90;
+
+void Excute(Canny* Op, uint8_t* GreyImageArray, uint8_t* GaussianImageArray, float* GradientPixels, float* MatrixPixels,
+    uint8_t* SegmentPixels, uint8_t* DoubleThrePixels, uint8_t* EdgeArray, int Width, int Height, ns& TotalGaussianTime,
+    ns& TotalGradientTime, ns& TotalReduceTime, ns& TotalDoubleThresholdTime, ns& TotalEdgeHysteresisTime)
+{
+    auto start = hrClk::now();
+    Op->PerformGaussianBlur(GaussianImageArray, GreyImageArray, Width, Height);
+    auto end = hrClk::now();
+    TotalGaussianTime += std::chrono::duration_cast<ns>(end - start);
+
+    start = hrClk::now();
+    Op->ComputeGradients(GradientPixels, SegmentPixels, GaussianImageArray, Width, Height);
+    end = hrClk::now();
+    TotalGradientTime += std::chrono::duration_cast<ns>(end - start);
+
+    start = hrClk::now();
+    Op->ReduceNonMaximum(MatrixPixels, GradientPixels, SegmentPixels, Width, Height);
+    end = hrClk::now();
+    TotalReduceTime += std::chrono::duration_cast<ns>(end - start);
+
+    start = hrClk::now();
+    Op->PerformDoubleThresholding(DoubleThrePixels, MatrixPixels, HighThre, LowThre, Width, Height);
+    end = hrClk::now();
+    TotalDoubleThresholdTime += std::chrono::duration_cast<ns>(end - start);
+
+    start = hrClk::now();
+    Op->PerformEdgeHysteresis(EdgeArray, DoubleThrePixels, Width, Height);
+    end = hrClk::now();
+    TotalEdgeHysteresisTime += std::chrono::duration_cast<ns>(end - start);
+}
+
+void PerformCanny(cv::Mat& EdgedImg, const cv::Mat& GreyImg, Canny* Op, std::ofstream& CSV, Str Filename,
+    unsigned int RepeatTimes = 1)
+{
+    std::string baseFilename   = Filename.substr(0, Filename.find_last_of("."));
+    std::string suffix         = ".jpg";
+    int         OriginalWidth  = GreyImg.cols;
+    int         OriginalHeight = GreyImg.rows;
+    int         TmpWidth       = GreyImg.cols < 16 ? 16 : GreyImg.cols / 16 * 16;
+    int         TmpHeight      = GreyImg.rows < 16 ? 16 : GreyImg.rows / 16 * 16;
+    cv::Mat     ResizedGreyImg;
+    cv::resize(GreyImg, ResizedGreyImg, cv::Size(TmpWidth, TmpHeight));
+    EdgedImg.create(ResizedGreyImg.size(), CV_8UC1);
+
+    cv::Mat GradientImg(ResizedGreyImg.size(), CV_8UC1);
+    cv::Mat MatrixImg(ResizedGreyImg.size(), CV_8UC1);
+    cv::Mat DoubleThreImg(ResizedGreyImg.size(), CV_8UC1);
+
+    uint8_t* GreyImageArray     = (uint8_t*)_mm_malloc(TmpWidth * TmpHeight * sizeof(uint8_t), 64);
+    uint8_t* GaussianImageArray = (uint8_t*)_mm_malloc(TmpWidth * TmpHeight * sizeof(uint8_t), 64);
+    float*   GradientPixels     = (float*)_mm_malloc(TmpWidth * TmpHeight * sizeof(float), 64);
+    float*   MatrixPixels       = (float*)_mm_malloc(TmpWidth * TmpHeight * sizeof(float), 64);
+    uint8_t* SegmentPixels      = (uint8_t*)_mm_malloc(TmpWidth * TmpHeight * sizeof(uint8_t), 64);
+    uint8_t* DoubleThrePixels   = (uint8_t*)_mm_malloc(TmpWidth * TmpHeight * sizeof(uint8_t), 64);
+    uint8_t* EdgeArray          = (uint8_t*)_mm_malloc(TmpWidth * TmpHeight * sizeof(uint8_t), 64);
+
+    ns TotalGaussianTime        = ns(0);
+    ns TotalGradientTime        = ns(0);
+    ns TotalReduceTime          = ns(0);
+    ns TotalDoubleThresholdTime = ns(0);
+    ns TotalEdgeHysteresisTime  = ns(0);
+
+    memcpy(GreyImageArray, ResizedGreyImg.data, TmpWidth * TmpHeight * sizeof(uint8_t));
+
+    for (int i = 0; i < 1; ++i)
+        Op->PerformGaussianBlur(GaussianImageArray, GreyImageArray, ResizedGreyImg.cols, ResizedGreyImg.rows);
+
+    for (int i = 0; i < RepeatTimes; ++i)
+    {
+        Excute(Op,
+            GreyImageArray,
+            GaussianImageArray,
+            GradientPixels,
+            MatrixPixels,
+            SegmentPixels,
+            DoubleThrePixels,
+            EdgeArray,
+            ResizedGreyImg.cols,
+            ResizedGreyImg.rows,
+            TotalGaussianTime,
+            TotalGradientTime,
+            TotalReduceTime,
+            TotalDoubleThresholdTime,
+            TotalEdgeHysteresisTime);
+    }
+    cv::Mat GaussianImg(ResizedGreyImg.size(), CV_8UC1, GaussianImageArray);
+    cv::imwrite(OutputPath + baseFilename + "_Gaussian" + suffix, GaussianImg);
+
+    for (int y = 0; y < ResizedGreyImg.rows; ++y)
+    {
+        for (int x = 0; x < ResizedGreyImg.cols; ++x)
+        {
+            GradientImg.at<uchar>(y, x) = static_cast<uchar>(GradientPixels[y * ResizedGreyImg.cols + x]);
+        }
+    }
+
+    cv::imwrite(OutputPath + baseFilename + "_Gradient" + suffix, GradientImg);
+
+    for (int y = 0; y < ResizedGreyImg.rows; ++y)
+    {
+        for (int x = 0; x < ResizedGreyImg.cols; ++x)
+        {
+            MatrixImg.at<uchar>(y, x) = static_cast<uchar>(MatrixPixels[y * ResizedGreyImg.cols + x]);
+        }
+    }
+
+    cv::imwrite(OutputPath + baseFilename + "_Matrix" + suffix, MatrixImg);
+
+    for (int y = 0; y < ResizedGreyImg.rows; ++y)
+    {
+        for (int x = 0; x < ResizedGreyImg.cols; ++x)
+        {
+            DoubleThreImg.at<uchar>(y, x) = static_cast<uchar>(DoubleThrePixels[y * ResizedGreyImg.cols + x]);
+        }
+    }
+
+    cv::imwrite(OutputPath + baseFilename + "_DoubleThre" + suffix, DoubleThreImg);
+
+    ns avgGaussianTime        = TotalGaussianTime / RepeatTimes;
+    ns avgGradientTime        = TotalGradientTime / RepeatTimes;
+    ns avgReductionTime       = TotalReduceTime / RepeatTimes;
+    ns avgDoubleThresholdTime = TotalDoubleThresholdTime / RepeatTimes;
+    ns avgEdgeHysteresisTime  = TotalEdgeHysteresisTime / RepeatTimes;
+    ns avgTotalProcessingTime =
+        avgGaussianTime + avgGradientTime + avgReductionTime + avgDoubleThresholdTime + avgEdgeHysteresisTime;
+
+    memcpy(EdgedImg.data, EdgeArray, TmpWidth * TmpHeight * sizeof(uint8_t));
+    cv::resize(EdgedImg, EdgedImg, cv::Size(OriginalWidth, OriginalHeight));
+    _mm_free(GreyImageArray);
+    _mm_free(GaussianImageArray);
+    _mm_free(GradientPixels);
+    _mm_free(MatrixPixels);
+    _mm_free(SegmentPixels);
+    _mm_free(DoubleThrePixels);
+    _mm_free(EdgeArray);
+    GreyImageArray     = nullptr;
+    GaussianImageArray = nullptr;
+    GradientPixels     = nullptr;
+    MatrixPixels       = nullptr;
+    SegmentPixels      = nullptr;
+    DoubleThrePixels   = nullptr;
+    EdgeArray          = nullptr;
+
+    if (!cv::imwrite(OutputPath + Filename, EdgedImg))
+    {
+        std::cerr << "Failed to save the image: " << OutputPath + Filename << std::endl;
+    }
+    else { std::cout << "Final image saved to " << OutputPath + Filename << std::endl; }
+
+    std::cout << "Processed " << Filename << " (" << ResizedGreyImg.cols << "x" << ResizedGreyImg.rows
+              << "): " << avgTotalProcessingTime.count() << "ns" << std::endl;
+
+    CSV << Filename << "," << ResizedGreyImg.cols << "x" << ResizedGreyImg.rows << "," << avgGaussianTime.count() << ","
+        << avgGradientTime.count() << "," << avgReductionTime.count() << "," << avgDoubleThresholdTime.count() << ","
+        << avgEdgeHysteresisTime.count() << "," << avgTotalProcessingTime.count() << "\n";
+}
+
 int main()
 {
     int n         = 1;
@@ -36,11 +197,6 @@ int main()
     std::cout << "How many threads to test? ";
     std::cin >> MaxThread;
 
-    Str       ImgPath    = "../Images/";
-    Str       OutputPath = "../Output/";
-    const int LowThre    = 30;
-    const int HighThre   = 90;
-
     Serial&          Serial_Ins   = Serial::GetInstance();
     SIMD::AVX::A512& SIMD_Ins     = SIMD::AVX::A512::GetInstance();
     PThread&         PThread_Ins  = PThread::GetInstance(MaxThread);
@@ -48,57 +204,16 @@ int main()
     OpenMP&          OMP_Ins      = OpenMP::GetInstance(MaxThread);
     OneAPI&          OneAPI_Ins   = OneAPI::GetInstance(MaxThread);
 
-    Canny* Gauss = nullptr;
-    Canny* Grad  = nullptr;
-    Canny* Redu  = nullptr;
-    Canny* DouTh = nullptr;
-    Canny* Edged = nullptr;
+    Canny* Op = nullptr;
 
     switch (Choice)
     {
-        case 1:
-            Gauss = &Serial_Ins;
-            Grad  = &Serial_Ins;
-            Redu  = &Serial_Ins;
-            DouTh = &Serial_Ins;
-            Edged = &Serial_Ins;
-            break;
-        case 2:
-            Gauss = &SIMD_Ins;
-            Grad  = &SIMD_Ins;
-            Redu  = &SIMD_Ins;
-            DouTh = &SIMD_Ins;
-            Edged = &SIMD_Ins;
-            break;
-        case 3:
-            Gauss = &PThread_Ins;
-            Grad  = &PThread_Ins;
-            Redu  = &PThread_Ins;
-            DouTh = &PThread_Ins;
-            Edged = &PThread_Ins;
-            break;
-        case 4:
-            Gauss = &PThreadP_Ins;
-            Grad  = &PThreadP_Ins;
-            Redu  = &PThreadP_Ins;
-            DouTh = &PThreadP_Ins;
-            Edged = &PThreadP_Ins;
-            break;
-        case 5:
-            Gauss = &OMP_Ins;
-            Grad  = &OMP_Ins;
-            Redu  = &OMP_Ins;
-            DouTh = &OMP_Ins;
-            Edged = &OMP_Ins;
-            break;
-        case 6:
-            Gauss = &OneAPI_Ins;
-            Grad  = &OneAPI_Ins;
-            Redu  = &OneAPI_Ins;
-            DouTh = &OneAPI_Ins;
-            ;
-            Edged = &OneAPI_Ins;
-            break;
+        case 1: Op = &Serial_Ins; break;
+        case 2: Op = &SIMD_Ins; break;
+        case 3: Op = &PThread_Ins; break;
+        case 4: Op = &PThreadP_Ins; break;
+        case 5: Op = &OMP_Ins; break;
+        case 6: Op = &OneAPI_Ins; break;
         default: std::cerr << "Invalid choice.\n"; return 1;
     }
 
@@ -131,144 +246,7 @@ int main()
 
             cv::Mat GreyImg, EdgedImg;
             cv::cvtColor(OriImg, GreyImg, cv::COLOR_BGR2GRAY);
-            int OriginalWidth  = GreyImg.cols;
-            int OriginalHeight = GreyImg.rows;
-            int TmpWidth       = GreyImg.cols < 16 ? 16 : GreyImg.cols / 16 * 16;
-            int TmpHeight      = GreyImg.rows < 16 ? 16 : GreyImg.rows / 16 * 16;
-            cv::resize(GreyImg, GreyImg, cv::Size(TmpWidth, TmpHeight));
-            EdgedImg.create(GreyImg.size(), CV_8UC1);
-
-            cv::Mat GaussianImg(GreyImg.size(), CV_8UC1);
-            cv::Mat GradientImg(GreyImg.size(), CV_8UC1);
-            cv::Mat MatrixImg(GreyImg.size(), CV_8UC1);
-            cv::Mat DoubleThreImg(GreyImg.size(), CV_8UC1);
-
-            uint8_t* GreyImageArray     = (uint8_t*)_mm_malloc(TmpWidth * TmpHeight * sizeof(uint8_t), 64);
-            uint8_t* GaussianImageArray = (uint8_t*)_mm_malloc(TmpWidth * TmpHeight * sizeof(uint8_t), 64);
-            float*   GradientPixels     = (float*)_mm_malloc(TmpWidth * TmpHeight * sizeof(float), 64);
-            float*   MatrixPixels       = (float*)_mm_malloc(TmpWidth * TmpHeight * sizeof(float), 64);
-            uint8_t* SegmentPixels      = (uint8_t*)_mm_malloc(TmpWidth * TmpHeight * sizeof(uint8_t), 64);
-            uint8_t* DoubleThrePixels   = (uint8_t*)_mm_malloc(TmpWidth * TmpHeight * sizeof(uint8_t), 64);
-            uint8_t* EdgeArray          = (uint8_t*)_mm_malloc(TmpWidth * TmpHeight * sizeof(uint8_t), 64);
-
-            memcpy(GreyImageArray, GreyImg.data, TmpWidth * TmpHeight * sizeof(uint8_t));
-
-            for (int i = 0; i < 1; ++i)
-            {
-                OMP_Ins.PerformGaussianBlur(GaussianImageArray, GreyImageArray, GreyImg.cols, GreyImg.rows);
-                cv::Mat GaussianImg(GreyImg.size(), CV_8UC1, GaussianImageArray);
-                cv::imwrite(OutputPath + "Gaussian_" + Entry.path().filename().string(), GaussianImg);
-            }
-
-            ns TotalGaussianTime        = ns(0);
-            ns TotalGradientTime        = ns(0);
-            ns TotalReduceTime          = ns(0);
-            ns TotalDoubleThresholdTime = ns(0);
-            ns TotalEdgeHysteresisTime  = ns(0);
-            ns TotalProcessingTime      = ns(0);
-
-            for (int i = 0; i < n; ++i)
-            {
-                auto start = hrClk::now();
-                Gauss->PerformGaussianBlur(GaussianImageArray, GreyImageArray, GreyImg.cols, GreyImg.rows);
-                auto end = hrClk::now();
-                TotalGaussianTime += std::chrono::duration_cast<ns>(end - start);
-
-                cv::Mat GaussianImg(GreyImg.size(), CV_8UC1, GaussianImageArray);
-                cv::imwrite(OutputPath + "Gaussian_" + Entry.path().filename().string(), GaussianImg);
-
-                start = hrClk::now();
-                Grad->ComputeGradients(GradientPixels, SegmentPixels, GaussianImageArray, GreyImg.cols, GreyImg.rows);
-                end = hrClk::now();
-                TotalGradientTime += std::chrono::duration_cast<ns>(end - start);
-
-                for (int y = 0; y < GreyImg.rows; ++y)
-                {
-                    for (int x = 0; x < GreyImg.cols; ++x)
-                    {
-                        GradientImg.at<uchar>(y, x) = static_cast<uchar>(GradientPixels[y * GreyImg.cols + x]);
-                    }
-                }
-
-                cv::imwrite(OutputPath + "Gradient_" + Entry.path().filename().string(), GradientImg);
-
-                start = hrClk::now();
-                Redu->ReduceNonMaximum(MatrixPixels, GradientPixels, SegmentPixels, GreyImg.cols, GreyImg.rows);
-                end = hrClk::now();
-                TotalReduceTime += std::chrono::duration_cast<ns>(end - start);
-
-                for (int y = 0; y < GreyImg.rows; ++y)
-                {
-                    for (int x = 0; x < GreyImg.cols; ++x)
-                    {
-                        MatrixImg.at<uchar>(y, x) = static_cast<uchar>(MatrixPixels[y * GreyImg.cols + x]);
-                    }
-                }
-
-                cv::imwrite(OutputPath + "Matrix_" + Entry.path().filename().string(), MatrixImg);
-
-                start = hrClk::now();
-                DouTh->PerformDoubleThresholding(
-                    DoubleThrePixels, MatrixPixels, HighThre, LowThre, GreyImg.cols, GreyImg.rows);
-                end = hrClk::now();
-                TotalDoubleThresholdTime += std::chrono::duration_cast<ns>(end - start);
-
-                for (int y = 0; y < GreyImg.rows; ++y)
-                {
-                    for (int x = 0; x < GreyImg.cols; ++x)
-                    {
-                        DoubleThreImg.at<uchar>(y, x) = static_cast<uchar>(DoubleThrePixels[y * GreyImg.cols + x]);
-                    }
-                }
-
-                cv::imwrite(OutputPath + "DoubleThre_" + Entry.path().filename().string(), DoubleThreImg);
-
-                start = hrClk::now();
-                Edged->PerformEdgeHysteresis(EdgeArray, DoubleThrePixels, GreyImg.cols, GreyImg.rows);
-                end = hrClk::now();
-                TotalEdgeHysteresisTime += std::chrono::duration_cast<ns>(end - start);
-
-                cv::Mat EdgedImg(GreyImg.size(), CV_8UC1, EdgeArray);
-                cv::imwrite(OutputPath + "Edged_" + Entry.path().filename().string(), EdgedImg);
-            }
-            memcpy(EdgedImg.data, EdgeArray, TmpWidth * TmpHeight * sizeof(uint8_t));
-            _mm_free(GreyImageArray);
-            _mm_free(GaussianImageArray);
-            _mm_free(GradientPixels);
-            _mm_free(MatrixPixels);
-            _mm_free(SegmentPixels);
-            _mm_free(DoubleThrePixels);
-            _mm_free(EdgeArray);
-            GreyImageArray     = nullptr;
-            GaussianImageArray = nullptr;
-            GradientPixels     = nullptr;
-            MatrixPixels       = nullptr;
-            SegmentPixels      = nullptr;
-            DoubleThrePixels   = nullptr;
-            EdgeArray          = nullptr;
-
-            ns avgGaussianTime        = TotalGaussianTime / n;
-            ns avgGradientTime        = TotalGradientTime / n;
-            ns avgReductionTime       = TotalReduceTime / n;
-            ns avgDoubleThresholdTime = TotalDoubleThresholdTime / n;
-            ns avgEdgeHysteresisTime  = TotalEdgeHysteresisTime / n;
-            ns avgTotalProcessingTime =
-                avgGaussianTime + avgGradientTime + avgReductionTime + avgDoubleThresholdTime + avgEdgeHysteresisTime;
-
-            cv::resize(EdgedImg, EdgedImg, cv::Size(OriginalWidth, OriginalHeight));
-            if (!cv::imwrite(OutputPath + Entry.path().filename().string(), EdgedImg))
-            {
-                std::cerr << "Failed to save the image: " << OutputPath + Entry.path().filename().string() << std::endl;
-            }
-            else { std::cout << "Final image saved to " << OutputPath + Entry.path().filename().string() << std::endl; }
-
-            std::cout << "Processed " << Entry.path().filename().string() << " (" << GreyImg.cols << "x" << GreyImg.rows
-                      << "): " << avgTotalProcessingTime.count() << "ns" << std::endl;
-
-            CSV << Entry.path().filename().string() << "," << GreyImg.cols << "x" << GreyImg.rows << ","
-                << avgGaussianTime.count() << "," << avgGradientTime.count() << "," << avgReductionTime.count() << ","
-                << avgDoubleThresholdTime.count() << "," << avgEdgeHysteresisTime.count() << ","
-                << avgTotalProcessingTime.count() << "\n";
+            PerformCanny(EdgedImg, GreyImg, Op, CSV, Entry.path().filename().string(), n);
         }
     }
     std::cout << "Thread " << MaxThread << " done.\n\n";
--- test.cpp
+++ test.cpp
@@ -1,37 +1,7 @@
-#include <tbb/flow_graph.h>
 #include <iostream>
+#include <oneapi/tbb.h>
 
-int main()
-{
-    // 创建一个图对象
-    tbb::flow::graph g;
-
-    // 创建一个可以发出数据的广播节点
-    tbb::flow::broadcast_node<int> inputNode(g);
-
-    // 创建一个可以接收数据并对其执行操作的函数节点
-    tbb::flow::function_node<int, int> printNode(g, 1, [](const int& n) {
-        std::cout << "Received " << n << std::endl;
-        return n;  // 返回接收到的数
-    });
-
-    // 创建一个可以接收数据并对其执行操作的函数节点，计算平方
-    tbb::flow::function_node<int, int> squareNode(g, 1, [](const int& n) {
-        std::cout << "Square of " << n << " is " << n * n << std::endl;
-        return n * n;  // 返回数的平方
-    });
-
-    // 将广播节点和函数节点连接起来，这意味着当广播节点发出数据时，函数节点会接收并处理它
-    tbb::flow::make_edge(inputNode, printNode);
-    tbb::flow::make_edge(printNode, squareNode);
-
-    // 让广播节点发送一些数据
-    inputNode.try_put(10);
-    inputNode.try_put(20);
-    inputNode.try_put(30);
-
-    // 等待所有任务完成
-    g.wait_for_all();
-
+int main() {
+    std::cout << "TBB Version: " << TBB_VERSION_MAJOR << "." << TBB_VERSION_MINOR << std::endl;
     return 0;
 }
